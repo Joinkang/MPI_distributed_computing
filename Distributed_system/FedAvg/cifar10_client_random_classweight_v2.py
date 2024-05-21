61523b2e52ae6488fc8b8e60ce11147f04b5b6ad
Joinkang <121596307+Joinkang@users.noreply.github.com>
2024-05-21 12:52:02 +0900

    Update cifar10_client_random_classweight_v2.py

--- Distributed_system/FedAvg/cifar10_client_random_classweight_v2.py
+++ Distributed_system/FedAvg/cifar10_client_random_classweight_v2.py
@@ -31,16 +31,15 @@ class_num = 10
 if rank != 1:
 
 
-    # CIFAR-10 데이터셋 로드
     (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
 
     num_class_for_sample = len(train_images)//class_num 
 
-    # 데이터 전처리: 이미지 픽셀 값을 0~1 범위로 조정
+
     train_images = train_images.astype('float32') / 255.0
     test_images = test_images.astype('float32') / 255.0
 
-    # 레이블을 one-hot 인코딩
+
     train_labels = to_categorical(train_labels, 10)
     test_labels = to_categorical(test_labels, 10)
 
